750263192156cb01d79837a574738474e3a2e6b2
Tomohiro Yoshida <tomohiroyoshida0511@gmail.com>
2020-10-17 14:21:09 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema");
+const resolvers = require("./resolvers");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
+
+const port = process.env.PORT || 4200;
+
+app.listen(port);
+
+console.log(`🚀 Server ready at http://localhost:4200/graphql`);
